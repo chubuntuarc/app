9378291a8409c5ca27c574498edc8adc27a448d9
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2018-04-05 22:09:48 -0600

    index
    Start page from initial beta project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>Cook your way - glib</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+      <link rel="stylesheet" href="../app/css/style.css">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link rel="shortcut icon" type="image/png" href="favicon.png"/>
+    </head>
+    <body style="background-color:#FF6624;">
+      <div class="row">
+        <div class="col s4 offset-s4 center-align">
+          <h1 class="brand-logo white-text">glib</h1>
+          <p class="white-text" style="margin-top:-20px;">Cook your way</p>
+        </div>
+      </div>
+      
+    </body>
+</html>
